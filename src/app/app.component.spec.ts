f51a27bd952033d9c019a53f75c4b3a30aa9007a
Tamas Horvath <horvathtamas22@gmail.com>
2019-12-23 09:01:34 +0100

    changed to SCSS

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'homework1'`, () => {
+  it(`should have as title 'homework-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('homework1');
+    expect(app.title).toEqual('homework-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to homework1!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to homework-app!');
   });
 });
