8a8ef1d77cea35097320d1686d61219b0ee73ddc
Tamas Horvath <horvathtamas22@gmail.com>
2019-12-24 11:12:08 +0100

    refactor

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,12 +1,13 @@
 export default class Utils {
 
     static createStringFromArray(array: Array<Object>, property?: any): String {
+        const comaAndSpace = ', ';
         let resultString = '';
         for (let i = 0; i < array.length; i++) {
             if (property) {
-                resultString += array[i][property] + ', ';
+                resultString += array[i][property] + comaAndSpace;
             } else {
-                resultString += array[i] + ', ';
+                resultString += array[i] + comaAndSpace;
             }
         }
         return resultString.substring(0, resultString.length - 2);
